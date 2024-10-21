f1418c1da78742b0de7370bdd1741d3da25002b8
İrem Rakıcı <iremrakici53@hotmail.com>
2024-10-21 13:16:07 +0300

    Proje dosyası ekle.

---
+++ Encapsulatıon/Car.cs
@@ -0,0 +1,84 @@
+﻿namespace Encapsulatıon
+{
+    public class Car
+    {
+        public Car(string brand, string model, string colour, int numberofDoors)
+        {
+            Brand = brand;
+            Model = model;
+            Colour = colour;
+            NumberofDoors = numberofDoors;
+        }
+        public void DisplayCarInfo()
+        {
+            Console.WriteLine("Marka: " + Brand);
+            Console.WriteLine("Model: " + Model);
+            Console.WriteLine("Renk: " + Colour);
+            Console.WriteLine("Kapı Sayısı:" + NumberofDoors);
+        }
+
+        private string _brand;
+        private string _model;
+        private string _colour;
+        private int _NumberofDoors;
+
+        public string Brand
+        {
+            get
+            {
+                return _brand;
+            }
+            set
+            {
+                _brand = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return _model;
+            }
+            set
+            {
+                _model = value;
+            }
+        }
+        public string Colour
+        {
+            get
+            {
+                return _colour;
+            }
+            set
+            {
+                _colour = value;
+            }
+        }
+        public int NumberofDoors
+        {
+            get
+            {
+                return _NumberofDoors;
+
+            }
+            set
+            {
+                if (value == 2 || value == 4)
+                {
+
+                    _NumberofDoors = value;
+
+                }
+                else
+                {
+                    Console.WriteLine($"Hatalı bir giriş yaptınız.Kapı sayısı sadece 2 veya 4 olabilir.Girilen değer:{value}");
+                    _NumberofDoors = -1;
+                }
+
+
+            }
+        }
+    }
+
+}
---
+++ Encapsulatıon/Program.cs
@@ -0,0 +1,19 @@
+﻿using Encapsulatıon;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car1 = new Car("Wolkswagen", "Polo", "Beyaz", 4);
+        car1.DisplayCarInfo();
+
+        Console.WriteLine("\r\n----------------------------------------------\r\n");
+
+        Car car2 = new Car("Opel", "Corsa", "Gri", 3);
+        car2.DisplayCarInfo();
+
+        Console.WriteLine("\r\n----------------------------------------------\r\n");
+
+
+    }
+}
